afb0c2b2fbdbdf0f37d3e97e1758ce467da635b0
Anders-H <anders@winsoft.se>
2022-06-27 18:05:56 +0200

    Added the backdrop for the terminal emulator.

---
+++ A BASIC Language/Gui/WinForms/TerminalEmulator.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace A_BASIC_Language.Gui.WinForms
+{
+    partial class TerminalEmulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TerminalEmulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "TerminalEmulator";
+            this.Text = "TerminalEmulator";
+            this.Shown += new System.EventHandler(this.TerminalEmulator_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TerminalEmulator_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TerminalEmulator_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TerminalEmulator_KeyPress);
+            this.Resize += new System.EventHandler(this.TerminalEmulator_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ A BASIC Language/Gui/WinForms/TerminalEmulator.cs
@@ -0,0 +1,257 @@
+﻿namespace A_BASIC_Language.Gui.WinForms;
+
+public partial class TerminalEmulator : Form
+{
+    private const int RowCount = 25;
+    private const int ColumnCount = 40;
+    private const int PixelsWidth = 320;
+    private const int PixelsHeight = 200;
+    private const int CharacterWidth = 8;
+    private const int CharacterHeight = 8;
+    private char[,] Characters;
+    private int CursorX { get; set; }
+    private int CursorY { get; set; }
+    private bool CursorBlink { get; set; }
+    private bool LineInputMode { get; set; }
+
+    public TerminalEmulator()
+    {
+        InitializeComponent();
+        Characters = new char[ColumnCount, RowCount];
+        CursorX = 0;
+        CursorY = 0;
+        LineInputMode = false;
+    }
+
+    private void TerminalEmulator_Shown(object sender, EventArgs e)
+    {
+        Font = new Font("Courier New", 6.0f);
+
+        Width = (int)(Screen.PrimaryScreen.WorkingArea.Width / 2.1);
+        Height = (int)(Screen.PrimaryScreen.WorkingArea.Height / 1.8);
+        CenterToScreen();
+
+        Invalidate();
+        timer1.Enabled = true;
+    }
+
+    public void Write(string text)
+    {
+        if (text == null!)
+            return;
+
+        foreach (var c in text)
+            WriteCharAndProgress(c);
+
+        OperationCompleted();
+    }
+
+    public void WriteLine(string text)
+    {
+        if (text == null!)
+            return;
+
+        foreach (var c in text)
+            WriteCharAndProgress(c);
+
+        CursorX = 0;
+        CursorY++;
+
+        if (CursorY >= RowCount)
+        {
+            CursorY = RowCount - 1;
+            ScrollUp();
+        }
+
+        OperationCompleted();
+    }
+
+    public void BeginLineInput()
+    {
+        LineInputMode = true;
+    }
+
+    private void OperationCompleted()
+    {
+        timer1.Enabled = false;
+        CursorBlink = true;
+        timer1.Enabled = true;
+        Invalidate();
+    }
+
+    private void KeyDownOperationCompleted(ref KeyEventArgs eventArgs)
+    {
+        eventArgs.SuppressKeyPress = true;
+        eventArgs.Handled = true;
+        timer1.Enabled = false;
+        CursorBlink = true;
+        timer1.Enabled = true;
+        Invalidate();
+    }
+
+    private void WriteCharAndProgress(char c)
+    {
+        Characters[CursorX, CursorY] = c;
+
+        CursorX++;
+
+        if (CursorX < ColumnCount)
+            return;
+
+        CursorX = 0;
+        CursorY++;
+
+        if (CursorY < RowCount)
+            return;
+
+        CursorY = RowCount - 1;
+        ScrollUp();
+    }
+
+    private void ScrollUp()
+    {
+        for (var y = RowCount - 1; y > 0; y--)
+        {
+            for (var x = 0; x < ColumnCount; x++)
+            {
+                Characters[x, y - 1] = Characters[x, y];
+            }
+        }
+
+        var lastRow = RowCount - 1;
+        var zeroChar = (char)0;
+
+        for (var x = 0; x < ColumnCount; x++)
+        {
+            Characters[x, lastRow] = zeroChar;
+        }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        CursorBlink = !CursorBlink;
+        Invalidate();
+    }
+
+    private void TerminalEmulator_Paint(object sender, PaintEventArgs e)
+    {
+        if (ClientRectangle.Width < 10 || ClientRectangle.Height < 10)
+            return;
+
+        if (ClientRectangle.Width > PixelsWidth && ClientRectangle.Height > PixelsHeight)
+        {
+            var scaleX = ClientRectangle.Width / (double)PixelsWidth;
+            var scaleY = ClientRectangle.Height / (double)PixelsHeight;
+            e.Graphics.ScaleTransform((float)scaleX, (float)scaleY);
+        }
+        else if (ClientRectangle.Width > PixelsWidth)
+        {
+            var scaleX = ClientRectangle.Width / (double)PixelsWidth;
+            e.Graphics.ScaleTransform((float)scaleX, 1.0f);
+        }
+        else if (ClientRectangle.Height > PixelsHeight)
+        {
+            var scaleY = ClientRectangle.Height / (double)PixelsHeight;
+            e.Graphics.ScaleTransform(1.0f, (float)scaleY);
+        }
+        else
+        {
+            e.Graphics.ScaleTransform(1.0f, 1.0f);
+        }
+
+        e.Graphics.FillRectangle(Brushes.Black, ClientRectangle);
+
+        var pixelX = 0;
+        var pixelY = 0;
+
+        for (var y = 0; y < RowCount; y++)
+        {
+            for (var x = 0; x < ColumnCount; x++)
+            {
+                if (CursorBlink && CursorX == x && CursorY == y)
+                {
+                    e.Graphics.FillRectangle(Brushes.Green, pixelX, pixelY, CharacterWidth, CharacterHeight);
+
+                    if (Characters[x, y] > 0)
+                        e.Graphics.DrawString(Characters[x, y].ToString(), Font, Brushes.Black, pixelX, pixelY);
+                }
+                else
+                {
+                    if (Characters[x, y] > 0)
+                        e.Graphics.DrawString(Characters[x, y].ToString(), Font, Brushes.Green, pixelX, pixelY);
+                }
+                pixelX += CharacterWidth;
+            }
+            pixelX = 0;
+            pixelY += CharacterHeight;
+        }
+    }
+
+    private void TerminalEmulator_Resize(object sender, EventArgs e)
+    {
+        Invalidate();
+    }
+
+    private void TerminalEmulator_KeyDown(object sender, KeyEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(e.KeyCode);
+        switch (e.KeyCode)
+        {
+            case Keys.Up:
+                CursorY--;
+
+                if (CursorY < 0)
+                    CursorY = 0;
+
+                KeyDownOperationCompleted(ref e);
+                break;
+            case Keys.Down:
+                CursorY++;
+
+                if (CursorY >= RowCount)
+                {
+                    CursorY = RowCount - 1;
+                    ScrollUp();
+                }
+
+                KeyDownOperationCompleted(ref e);
+                break;
+            case Keys.Left:
+                CursorX--;
+
+                if (CursorX < 0 && CursorY > 0)
+                {
+                    CursorX = ColumnCount - 1;
+                    CursorY--;
+                }
+                else if (CursorX < 0)
+                {
+                    CursorX = 0;
+                }
+
+                KeyDownOperationCompleted(ref e);
+                break;
+            case Keys.Right:
+                CursorX++;
+
+                if (CursorX >= ColumnCount && CursorY < RowCount - 1)
+                {
+                    CursorX = 0;
+                    CursorY++;
+                }
+                else if (CursorX >= ColumnCount)
+                {
+                    CursorX = 0;
+                    CursorY = RowCount - 1;
+                }
+
+                KeyDownOperationCompleted(ref e);
+                break;
+        }
+    }
+
+    private void TerminalEmulator_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine((int)e.KeyChar);
+    }
+}
