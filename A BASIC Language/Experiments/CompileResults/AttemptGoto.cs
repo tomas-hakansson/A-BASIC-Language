f319766af019cdecb736274058ea38df1e923a1a
Tomas Håkansson <tomas@pennybridge.org>
2022-06-30 22:45:53 +0200

    A couple of compiler result experiments

---
+++ A BASIC Language/Experiments/CompileResults/AttemptGoto.cs
@@ -0,0 +1,48 @@
+﻿namespace A_BASIC_Language.Experiments.CompileResults;
+internal class AttemptGoto
+{
+    /*
+     * This is an attempt to figure out what a compiled program could look like.
+     * I'm hoping this design would support some of the more dynamic goto features of BASIC.
+     * Note: This is just a sketch. I will not give it any real test yet.
+     */
+    public void ABLMain()
+    {
+    /* example program
+     * 5 input n
+     * 10 print n
+     * 15 print n + 1
+     * 20 print n + 5
+     * 25 goto n
+     */
+
+    //Note: I'm using console here because of convenience.
+    ABL_Label_5:
+        var readValue = Console.ReadLine();
+    ABL_Label_10:
+        var readValue_Int = int.Parse(readValue!);
+        Console.WriteLine(readValue_Int);
+    ABL_Label_15:
+        Console.WriteLine(readValue_Int + 1);
+    ABL_Label_20:
+        Console.WriteLine(readValue_Int + 5);
+    ABL_Label_25:
+        goto ABL_Label_GOTO_Switch;
+    ABL_Label_GOTO_Switch:
+        switch (readValue_Int)
+        {
+            case 5:
+                goto ABL_Label_5;
+            case 10:
+                goto ABL_Label_10;
+            case 15:
+                goto ABL_Label_15;
+            case 20:
+                goto ABL_Label_20;
+            case 25:
+                goto ABL_Label_25;
+            default:
+                throw new SystemException("invalid goto call");
+        }
+    }
+}
