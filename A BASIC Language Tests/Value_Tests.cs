3eef79b8e7c648ffb2dc47c2ae186c384eac7887
Anders-H <anders@winsoft.se>
2022-07-18 11:59:08 +0200

    Implicit cast from FLOAT works.

--- A BASIC Language Tests/Value_Tests.cs
+++ A BASIC Language Tests/Value_Tests.cs
@@ -25,15 +25,33 @@ public class Value_Tests
         var i = new IntValue(40);
 
         Assert.IsTrue(i.CanGetAsType<IntValue>());
-        Assert.IsTrue(i.GetValueAsType<IntValue>().GetType() == typeof(int));
+        Assert.IsTrue(i.GetValueAsType<IntValue>() is int);
         Assert.IsTrue((int)i.GetValueAsType<IntValue>() == 40);
 
         Assert.IsTrue(i.CanGetAsType<FloatValue>());
-        Assert.IsTrue(i.GetValueAsType<FloatValue>().GetType() == typeof(double));
+        Assert.IsTrue(i.GetValueAsType<FloatValue>() is double);
         Assert.IsTrue((double)i.GetValueAsType<FloatValue>() == 40.0);
 
         Assert.IsTrue(i.CanGetAsType<StringValue>());
-        Assert.IsTrue(i.GetValueAsType<StringValue>().GetType() == typeof(string));
+        Assert.IsTrue(i.GetValueAsType<StringValue>() is string);
         Assert.IsTrue((string)i.GetValueAsType<StringValue>() == "40");
+    }
+
+    [TestMethod]
+    public void CanCastFromFloat()
+    {
+        var f = new FloatValue(40.5);
+
+        Assert.IsTrue(f.CanGetAsType<IntValue>());
+        Assert.IsTrue(f.GetValueAsType<IntValue>() is int);
+        Assert.IsTrue((int)f.GetValueAsType<IntValue>() == 40);
+
+        Assert.IsTrue(f.CanGetAsType<FloatValue>());
+        Assert.IsTrue(f.GetValueAsType<FloatValue>() is double);
+        Assert.IsTrue((double)f.GetValueAsType<FloatValue>() == 40.5);
+
+        Assert.IsTrue(f.CanGetAsType<StringValue>());
+        Assert.IsTrue(f.GetValueAsType<StringValue>() is string);
+        Assert.IsTrue((string)f.GetValueAsType<StringValue>() == "40.5");
     }
 }
