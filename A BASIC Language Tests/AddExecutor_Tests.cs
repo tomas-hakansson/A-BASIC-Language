57b4e14e22e255b766b4b41f5aea773718eedfa8
Anders-H <anders@winsoft.se>
2022-08-12 13:52:46 +0200

    Keeping Tomas' code and adding som formats.

--- A BASIC Language Tests/AddExecutor_Tests.cs
+++ A BASIC Language Tests/AddExecutor_Tests.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace A_BASIC_Language_Tests;
 
 [TestClass]
-public class Executor_Tests
+public class AddExecutor_Tests
 {
     [TestMethod]
     public void Add()
@@ -67,66 +67,5 @@ public class Executor_Tests
         executor.Run();
         Assert.AreEqual(values.Count, 1);
         Assert.AreEqual(((dynamic)values.Pop()).Value, 3);
-    }
-
-    [TestMethod]
-    public void Add2()
-    {
-        Stack<ValueBase> values = new();
-        AddExecutor executor = new(values);
-
-        values.Push(new FloatValue(1));
-        values.Push(new FloatValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, 3);
-
-        values.Push(new FloatValue(1));
-        values.Push(new IntValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, 3);
-
-        values.Push(new FloatValue(1));
-        values.Push(new StringValue("2"));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, "21");
-
-        values.Push(new IntValue(1));
-        values.Push(new IntValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, 3);
-
-        values.Push(new IntValue(1));
-        values.Push(new FloatValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, 3);
-
-        values.Push(new IntValue(1));
-        values.Push(new StringValue("2"));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, "21");
-
-        values.Push(new StringValue("1"));
-        values.Push(new StringValue("2"));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, "21");
-
-        values.Push(new StringValue("1"));
-        values.Push(new FloatValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, "21");
-
-        values.Push(new StringValue("1"));
-        values.Push(new IntValue(2));
-        executor.Run2();
-        Assert.AreEqual(values.Count, 1);
-        Assert.AreEqual(((dynamic)values.Pop()).Value, "21");
     }
 }
--- A BASIC Language/SpecificExecutors/AddExecutor.cs
+++ A BASIC Language/SpecificExecutors/AddExecutor.cs
@@ -15,71 +15,6 @@ public class AddExecutor : ISubExecutor
     {
         if (_data.Count >= 2)
         {
-            var x = _data.Pop();
-            var y = _data.Pop();
-
-            if (x.IsOfType<FloatValue>())
-            {
-                if (y.CanGetAsType<FloatValue>())
-                {
-                    var left = (double)x.GetValueAsType<FloatValue>();
-                    var right = (double)y.GetValueAsType<FloatValue>();
-                    _data.Push(new FloatValue(left + right));
-                }
-                else if (y.CanGetAsType<IntValue>())
-                {
-                    var left = (double)x.GetValueAsType<FloatValue>();
-                    var right = (int)y.GetValueAsType<IntValue>();
-                    _data.Push(new FloatValue(left + right));
-                }
-                else if (y.CanGetAsType<StringValue>() && x.CanGetAsType<StringValue>())
-                {
-                    var left = (string)x.GetValueAsType<StringValue>();
-                    var right = (string)y.GetValueAsType<StringValue>();
-                    _data.Push(new StringValue(left + right));
-                }
-            }
-            else if (x.IsOfType<IntValue>())
-            {
-                if (y.CanGetAsType<FloatValue>())
-                {
-                    var left = (int)x.GetValueAsType<IntValue>();
-                    var right = (double)y.GetValueAsType<FloatValue>();
-                    _data.Push(new FloatValue(left + right));
-                }
-                else if (y.CanGetAsType<IntValue>())
-                {
-                    var left = (int)x.GetValueAsType<IntValue>();
-                    var right = (int)y.GetValueAsType<IntValue>();
-                    _data.Push(new FloatValue(left + right));
-                }
-                else if (y.CanGetAsType<StringValue>() && x.CanGetAsType<StringValue>())
-                {
-                    var left = (string)x.GetValueAsType<StringValue>();
-                    var right = (string)y.GetValueAsType<StringValue>();
-                    _data.Push(new StringValue(left + right));
-                }
-            }
-            else if (x.IsOfType<StringValue>() && y.CanGetAsType<StringValue>())
-            {
-                var left = (string)x.GetValueAsType<StringValue>();
-                var right = (string)y.GetValueAsType<StringValue>();
-                _data.Push(new StringValue(left + right));
-            }
-            else
-            {
-                throw new SystemException("What?");
-            }
-        }
-        else
-            throw new SystemException("Insufficient items on the stack.");
-
-    }
-
-    public void Run2()
-    {
-        if (_data.Count >= 2)
-        {
             dynamic x = _data.Pop();
             dynamic y = _data.Pop();
             _data.Push(Add(x, y));
@@ -89,15 +24,36 @@ public class AddExecutor : ISubExecutor
 
     }
 
-    static ValueBase Add(FloatValue x, FloatValue y) => new FloatValue(x.Value + y.Value);
-    static ValueBase Add(FloatValue x, IntValue y) => new FloatValue(x.Value + y.Value);
-    static ValueBase Add(FloatValue x, StringValue y) => new StringValue(x.Value + y.Value);
+    // Float rules
 
-    static ValueBase Add(IntValue x, IntValue y) => new FloatValue(x.Value + y.Value);
-    static ValueBase Add(IntValue x, FloatValue y) => new FloatValue(x.Value + y.Value);
-    static ValueBase Add(IntValue x, StringValue y) => new StringValue(x.Value + y.Value);
+    static ValueBase Add(FloatValue x, FloatValue y) =>
+        new FloatValue(x.Value + y.Value);
 
-    static ValueBase Add(StringValue x, StringValue y) => new StringValue(x.Value + y.Value);
-    static ValueBase Add(StringValue x, FloatValue y) => new StringValue(x.Value + y.Value);
-    static ValueBase Add(StringValue x, IntValue y) => new StringValue(x.Value + y.Value);
+    static ValueBase Add(FloatValue x, IntValue y) =>
+        new FloatValue(x.Value + y.Value);
+
+    static ValueBase Add(FloatValue x, StringValue y) =>
+        new StringValue(x.Value + y.Value);
+
+    // Int rules
+
+    static ValueBase Add(IntValue x, IntValue y) =>
+        new FloatValue(x.Value + y.Value);
+
+    static ValueBase Add(IntValue x, FloatValue y) =>
+        new FloatValue(x.Value + y.Value);
+
+    static ValueBase Add(IntValue x, StringValue y) =>
+        new StringValue(x.Value + y.Value);
+
+    // String rules
+
+    static ValueBase Add(StringValue x, StringValue y) =>
+        new StringValue(x.Value + y.Value);
+
+    static ValueBase Add(StringValue x, FloatValue y) =>
+        new StringValue(x.Value + y.Value);
+
+    static ValueBase Add(StringValue x, IntValue y) =>
+        new StringValue(x.Value + y.Value);
 }
