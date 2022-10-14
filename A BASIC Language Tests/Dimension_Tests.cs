2a2f5eb472b04eb53570862f97507a4d411237f8
Tomas Håkansson <tomas@pennybridge.org>
2022-10-14 19:42:02 +0200

    Added multidimensional array type
    This allows you to have arrays of different shapes in the same collection.

---
+++ A BASIC Language Tests/Dimension_Tests.cs
@@ -0,0 +1,35 @@
+﻿using A_BASIC_Language;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using A_BASIC_Language.ValueTypes;
+
+namespace A_BASIC_Language_Tests
+{
+    [TestClass]
+    public class Dimension_Tests
+    {
+        [TestMethod]
+        public void blah()
+        {
+            //int[,] a1 = new int[2, 4];
+            //int[,] a2 = new int[3, 4];
+            //int[,,] a3 = new int[2, 3, 4];
+            //List<DIM> ases = new();
+            var shape = new List<int>() { 1, 2 };
+            Dimension dim = new(shape);
+            dim.Add(10, 0, 0);
+            dim.Add(11, 0, 1);
+            dim.Add(12, 0, 2);
+            dim.Add(13, 1, 0);
+            dim.Add(14, 1, 1);
+            dim.Add(15, 1, 2);
+
+            var value11 = dim.Get(1, 0);
+            //ases.Add(new DIM(a1, new int[] {2, 4}));
+            //ases.Add(new DIM(a3, new int[3] {2, 3, 4}));
+            //ases.Add(a1);
+            //ases.Add(a2);
+            //ases.Add(a3);
+        }
+    }
+}
---
+++ A BASIC Language/Dimension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using A_BASIC_Language.ValueTypes;
+
+namespace A_BASIC_Language
+{
+    //Note: I used the following for index calculation: https://stackoverflow.com/a/3613492
+    public class Dimension
+    {
+        readonly ValueBase[] _atoms;
+        readonly int[] _multipliers;
+        public Dimension(List<int> maxIndices)
+        {
+            //ToDo: Validate input.
+            var shape = maxIndices.Select(x => x + 1).ToList();
+            _atoms = new ValueBase[shape.Aggregate((x, y) => x * y)];
+            var sc = shape.Count;
+            _multipliers = new int[sc];
+            for (int i = 0, j = sc - 1; i < sc; i++, j--)
+                if (i == 0)
+                    _multipliers[j] = 1;
+                else
+                    _multipliers[j] = (int)Math.Pow(shape[i], i);
+        }
+
+        /// <summary>
+        /// This method is test only. It is hardcoded to only create IntValues and it has a params parameter.
+        /// </summary>
+        /// <param name="atom"></param>
+        /// <param name="indices"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Add(int atom, params int[] indices) => Add(new IntValue(atom), indices.ToList());
+
+        public void Add(double atom, List<int> indices) => Add(new FloatValue(atom), indices.ToList());
+
+        public void Add(int atom, List<int> indices) => Add(new IntValue(atom), indices.ToList());
+
+        public void Add(string atom, List<int> indices) => Add(new StringValue(atom), indices.ToList());
+
+        public void Add(ValueBase atom, List<int> indices) => _atoms[ToOnedimensionalIndex(indices)] = atom;
+
+        /// <summary>
+        /// This method is test only.
+        /// </summary>
+        /// <param name="indices"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int Get(params int[] indices) => (int)Get(indices.ToList()).GetValueAsType<IntValue>();
+
+        public ValueBase Get(List<int> indices) => _atoms[ToOnedimensionalIndex(indices)];
+
+        int ToOnedimensionalIndex(List<int> indices)
+        {
+            //ToDo: Validate input.
+            return _multipliers.Zip(indices, (x, y) => x * y).Aggregate((x, y) => x + y);
+        }
+    }
+}
