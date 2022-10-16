61f9b204eb6ca7605665b3071113377136411cc1
Anders-H <anders@winsoft.se>
2022-10-16 19:03:51 +0200

    Reduced the program class.

--- A BASIC Language/Program.cs
+++ A BASIC Language/Program.cs
@@ -1,16 +1,7 @@
 ﻿using A_BASIC_Language.Gui;
 
-namespace A_BASIC_Language;
-
-public static class Program
-{
-    [STAThread]
-    private static void Main(string[] args)
-    {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        var main = new MainWindow();
-        main.ProgramFilename = args.FirstOrDefault() ?? "";
-        Application.Run(main);
-    }
-}
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
+var main = new MainWindow();
+main.ProgramFilename = args.FirstOrDefault() ?? "";
+Application.Run(main);
