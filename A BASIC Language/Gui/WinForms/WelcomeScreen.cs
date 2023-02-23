4bacd9ff5c77fc3d29428432db92d3d57aca6362
Anders Hesselbom <anders@winsoft.se>
2023-02-23 20:45:03 +0100

    Welcome screen.

--- A BASIC Language/Gui/WinForms/WelcomeScreen.cs
+++ A BASIC Language/Gui/WinForms/WelcomeScreen.cs
@@ -15,7 +15,7 @@ public class WelcomeScreen
         _writeSeparator = writeSeparator;
     }
 
-    public void Show(string program)
+    public void Show(string programFilename)
     {
         var spaces = 28;
         switch (_columnCount)
@@ -33,7 +33,7 @@ public class WelcomeScreen
         _writeEmptyLine();
         _writeLine($"{new string(' ', spaces + 1)}Altair BASIC Emulator.");
 
-        if (string.IsNullOrWhiteSpace(program))
+        if (string.IsNullOrWhiteSpace(programFilename))
         {
             _writeLine($"{new string(' ', spaces - 1)}written by Tomas Hakansson");
             _writeLine($"{new string(' ', spaces + 2)}and Anders Hesselbom");
@@ -44,12 +44,12 @@ public class WelcomeScreen
             _writeEmptyLine();
         }
 
-        if (!string.IsNullOrWhiteSpace(program))
+        if (!string.IsNullOrWhiteSpace(programFilename))
         {
             _writeLine("Ready.");
             _writeEmptyLine();
-            _writeLine("Loaded program:");
-            _writeLine(program);
+            _writeLine("Loaded programFilename:");
+            _writeLine(programFilename);
             _writeSeparator();
         }
         else
